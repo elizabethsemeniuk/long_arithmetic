dfdd81bdb2214e1ce8c9bc352efaa11d0ba67716
elizabethsemeniuk <lizznight@gmail.com>
2020-11-03 03:26:50 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/long_arithmetic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/long_arithmetic.dir/main.cpp.obj"
+  "long_arithmetic.exe"
+  "long_arithmetic.exe.manifest"
+  "long_arithmetic.lib"
+  "long_arithmetic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/long_arithmetic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
